50d4044b4f3f50b9f9548cb4810b7599ed03b7fd
Marta Romero GarcíaRubio <marta.romero.gr29@gmail.com>
2019-09-09 10:20:21 +0200

    Add files via upload

---
+++ FRONT/CyberopsOperator/src/app/energia/energia.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EnergiaPage } from './energia.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EnergiaPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [EnergiaPage]
+})
+export class EnergiaPageModule {}
---
+++ FRONT/CyberopsOperator/src/app/energia/energia.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-energia',
+  templateUrl: './energia.page.html',
+  styleUrls: ['./energia.page.scss'],
+})
+export class EnergiaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
